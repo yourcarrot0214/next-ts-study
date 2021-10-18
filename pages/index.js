017a36fbe128a9b3244d1390052716380cb3df18
yourcarrot0214 <stylenbs@gmail.com>
2021-10-18 21:28:06 +0900

    2.2 정적페이지 라우팅 및 Link 컴포넌트

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
+import Tomato from "./tomato";
 
 const App = () => {
   return (
     <div>
       <h2>Link to 'tomato' Page</h2>
-      <Link href="/tomato">
+      <Link href="/tomato" as="토마토">
         <a>Move to 'tomato'</a>
       </Link>
     </div>

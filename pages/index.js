5e542fa290edd6b1d20116c85a2519e9889ea73b
yourcarrot0214 <stylenbs@gmail.com>
2021-10-19 15:04:44 +0900

    2.2 정적 파일 사용하기

--- pages/index.js
+++ pages/index.js
@@ -7,18 +7,7 @@ const App = () => {
 
   return (
     <div>
-      <button type="button" onClick={() => router.push("/tomato")}>
-        tomato로 가기
-      </button>
-      <p>이름</p>
-      <input
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        style={{ marginRight: "12px" }}
-      />
-      <button type="button" onClick={() => router.push(`/vegetable/${name}`)}>
-        {name}으로 가기
-      </button>
+      <img src="/favicon.ico" alt="favicon" />
     </div>
   );
 };
